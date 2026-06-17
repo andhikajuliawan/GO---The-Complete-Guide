8c66c893932fd00254fe73512ca25773debcbf60
andhika juliawan <andhikajuliawan@andhikas-MacBook-Air.local>
2026-06-17 16:45:53 +0700

    Working with Arrays

--- lists.go
+++ lists.go
@@ -9,6 +9,11 @@ type product struct {
 }
 
 func main() {
+	var productNames [4]string = [4]string{"A Book"}
 	prices := [4]float64{1.9, 2.9, 3.9, 4.9}
 	fmt.Println(prices)
+
+	productNames[2] = "A Carpet"
+	fmt.Println(productNames)
+	fmt.Println(prices[2])
 }
