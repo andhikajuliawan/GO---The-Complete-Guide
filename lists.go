d18fdca2cc5e4ba4118dbda9efa6d80941aab501
andhika juliawan <andhikajuliawan@andhikas-MacBook-Air.local>
2026-06-18 09:33:09 +0700

    More Ways Of Selecting Slices

--- lists.go
+++ lists.go
@@ -17,6 +17,7 @@ func main() {
 	fmt.Println(productNames)
 	fmt.Println(prices[2])
 
-	featuredPrices := prices[1:3]
-	fmt.Println(featuredPrices)
+	featuredPrices := prices[1:]
+	highlightedPrices := featuredPrices[:1]
+	fmt.Println(highlightedPrices)
 }
