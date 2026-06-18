ab64cb12bf8d99e12dcc10b6f69d11dfa961e57a
andhika juliawan <andhikajuliawan@andhikas-MacBook-Air.local>
2026-06-18 10:09:37 +0700

    Building Dynamic Lists With Slices

--- lists.go
+++ lists.go
@@ -9,22 +9,33 @@ type product struct {
 }
 
 func main() {
-	var productNames [4]string = [4]string{"A Book"}
-	prices := [4]float64{1.9, 2.9, 3.9, 4.9}
+	prices := []float64{1.9, 2.9}
+	fmt.Println(prices[0:1])
+	prices[1] = 3.9
+
+	prices = append(prices, 4.9)
 	fmt.Println(prices)
-
-	productNames[2] = "A Carpet"
-	fmt.Println(productNames)
-	fmt.Println(prices[2])
-
-	featuredPrices := prices[1:]
-	featuredPrices[0] = 199.99
-	highlightedPrices := featuredPrices[:1]
-	fmt.Println(highlightedPrices)
+	prices = prices[1:]
 	fmt.Println(prices)
-	fmt.Println(len(highlightedPrices), cap(highlightedPrices))
-
-	highlightedPrices = highlightedPrices[:3]
-	fmt.Println(highlightedPrices)
-	fmt.Println(len(highlightedPrices), cap(highlightedPrices))
 }
+
+// func main() {
+// 	var productNames [4]string = [4]string{"A Book"}
+// 	prices := [4]float64{1.9, 2.9, 3.9, 4.9}
+// 	fmt.Println(prices)
+
+// 	productNames[2] = "A Carpet"
+// 	fmt.Println(productNames)
+// 	fmt.Println(prices[2])
+
+// 	featuredPrices := prices[1:]
+// 	featuredPrices[0] = 199.99
+// 	highlightedPrices := featuredPrices[:1]
+// 	fmt.Println(highlightedPrices)
+// 	fmt.Println(prices)
+// 	fmt.Println(len(highlightedPrices), cap(highlightedPrices))
+
+// 	highlightedPrices = highlightedPrices[:3]
+// 	fmt.Println(highlightedPrices)
+// 	fmt.Println(len(highlightedPrices), cap(highlightedPrices))
+// }
