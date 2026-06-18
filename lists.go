38454aec13180e210a4369abe0bd12d54b61ee1f
andhika juliawan <andhikajuliawan@andhikas-MacBook-Air.local>
2026-06-18 09:54:46 +0700

    Diving Deeper Into Slices

--- lists.go
+++ lists.go
@@ -18,6 +18,13 @@ func main() {
 	fmt.Println(prices[2])
 
 	featuredPrices := prices[1:]
+	featuredPrices[0] = 199.99
 	highlightedPrices := featuredPrices[:1]
 	fmt.Println(highlightedPrices)
+	fmt.Println(prices)
+	fmt.Println(len(highlightedPrices), cap(highlightedPrices))
+
+	highlightedPrices = highlightedPrices[:3]
+	fmt.Println(highlightedPrices)
+	fmt.Println(len(highlightedPrices), cap(highlightedPrices))
 }
