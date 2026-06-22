23d31cc3d3f4da7f681c3983b136f095cf2f2169
andhika juliawan <andhikajuliawan@andhikas-MacBook-Air.local>
2026-06-22 10:36:44 +0700

    Working with Type Aliases

--- main.go
+++ main.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+type floatMap map[string]float64
+
+func (m floatMap) output() {
+	fmt.Println(m)
+}
+
 func main() {
 
 	// userName := []string{}
@@ -15,12 +21,12 @@ func main() {
 	fmt.Println(userName)
 
 	// courseRatings := map[string]float64{}
-	courseRatings := make(map[string]float64, 3)
+	courseRatings := make(floatMap, 3)
 
 	courseRatings["Go"] = 7.8
 	courseRatings["JS"] = 4.6
 	courseRatings["Angular"] = 4.7
 
-	fmt.Println(courseRatings)
-
+	courseRatings.output()
+	// fmt.Println(courseRatings)
 }
