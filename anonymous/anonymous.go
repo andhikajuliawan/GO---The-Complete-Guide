ca126c662efe871a8add2f23e34d4c9d940de00e
andhika juliawan <andhikajuliawan@andhikas-MacBook-Air.local>
2026-06-23 15:49:02 +0700

    Making Sense Of Recursion

---
+++ anonymous/anonymous.go
@@ -0,0 +1,42 @@
+package anonymous
+
+import (
+	"fmt"
+)
+
+type transformFn func(int) int
+
+func main() {
+	numbers := []int{1, 2, 3, 4, 5}
+
+	transformed := transformNumber(&numbers, func(val int) int {
+		return val * 2
+	})
+
+	fmt.Println(transformed)
+
+	double := createTransformer(2)
+	triple := createTransformer(3)
+
+	doubled := transformNumber(&numbers, double)
+	tripled := transformNumber(&numbers, triple)
+
+	fmt.Println(doubled)
+	fmt.Println(tripled)
+}
+
+func transformNumber(numbers *[]int, transform transformFn) []int {
+	result := []int{}
+
+	for _, val := range *numbers {
+		result = append(result, transform(val))
+	}
+
+	return result
+}
+
+func createTransformer(factor int) func(int) int {
+	return func(number int) int {
+		return number * factor
+	}
+}
--- main.go
+++ main.go
@@ -1,42 +1,17 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 type transformFn func(int) int
 
 func main() {
-	numbers := []int{1, 2, 3, 4, 5}
-
-	transformed := transformNumber(&numbers, func(val int) int {
-		return val * 2
-	})
-
-	fmt.Println(transformed)
-
-	double := createTransformer(2)
-	triple := createTransformer(3)
-
-	doubled := transformNumber(&numbers, double)
-	tripled := transformNumber(&numbers, triple)
-
-	fmt.Println(doubled)
-	fmt.Println(tripled)
+	fact := factorial(5)
+	fmt.Println(fact)
 }
 
-func transformNumber(numbers *[]int, transform transformFn) []int {
-	result := []int{}
-
-	for _, val := range *numbers {
-		result = append(result, transform(val))
+func factorial(number int) int {
+	if number == 0 {
+		return 1
 	}
-
-	return result
-}
-
-func createTransformer(factor int) func(int) int {
-	return func(number int) int {
-		return number * factor
-	}
+	return number * factorial(number-1)
 }
