e547507c05ccc46703f058bdfd2b02498c40690e
andhika juliawan <andhikajuliawan@andhikas-MacBook-Air.local>
2026-06-23 16:24:30 +0700

    Splitting Slices Into Parameter Values

--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	// numbers := []int{1, 5, 10}
+	numbers := []int{1, 5, 10}
 
 	sum := sumup(1, 10, 15, 40, -5)
+	anotherSum := sumup(1, numbers...)
 
 	fmt.Println(sum)
+	fmt.Println(anotherSum)
 }
 
 func sumup(startingValue int, numbers ...int) int {
