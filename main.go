0152245d3f4a517f4f1dd131fef61eafc78a0f66
andhika juliawan <andhikajuliawan@andhikas-MacBook-Air.local>
2026-06-24 16:35:32 +0700

    Introducing Goroutines

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func greet(phrase string) {
+	fmt.Println("Hello!", phrase)
+}
+
+func slowGreet(phrase string) {
+	time.Sleep(3 * time.Second)
+	fmt.Println("Hello!", phrase)
+}
+
+func main() {
+	greet("Nice to meet you!")
+	greet("How are you?")
+	slowGreet("How ... are ... you ...?")
+	greet("I hope you're liking the course!")
+}
