507f13181d0dc2a715d2acc9caa009be79d842a4
andhika juliawan <andhikajuliawan@andhikas-MacBook-Air.local>
2026-06-25 13:51:03 +0700

    Setting Up An Error Channel

--- main.go
+++ main.go
@@ -9,18 +9,24 @@ import (
 func main() {
 	taxRates := []float64{0, 0.07, 0.1, 0.15}
 	doneChans := make([]chan bool, len(taxRates))
+	errorChans := make([]chan error, len(taxRates))
 
 	for i, taxRate := range taxRates {
 		fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
 		// cmdm := cmdmanager.New()
 		priceJob := prices.NewTaxIncludedPriceJob(fm, taxRate)
 		doneChans[i] = make(chan bool)
-		go priceJob.Process(doneChans[i])
+		errorChans[i] = make(chan error)
+		go priceJob.Process(doneChans[i], errorChans[i])
 
 		// if err != nil {
 		// 	fmt.Println("Could not process job")
 		// 	fmt.Println(err)
 		// }
+	}
+
+	for _, errorChan := range errorChans {
+		<-errorChan
 	}
 
 	for _, doneChan := range doneChans {
--- prices/prices.go
+++ prices/prices.go
@@ -31,11 +31,11 @@ func (job *TaxIncludedPriceJob) LoadData() error {
 	return nil
 }
 
-func (job *TaxIncludedPriceJob) Process(doneChan chan bool) {
+func (job *TaxIncludedPriceJob) Process(doneChan chan bool, errorChan chan error) {
 	err := job.LoadData()
 
 	if err != nil {
-		// return err
+		errorChan <- err
 	}
 
 	result := make(map[string]string)
