ee2b1a98e0cc99092abc225460d92597d9083fc6
andhika juliawan <andhikajuliawan@andhikas-MacBook-Air.local>
2026-07-02 21:08:47 +0700

    Setting Up A First Route & Handling a First Request

--- main.go
+++ main.go
@@ -1,9 +1,21 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	server := gin.Default()
 
+	server.GET("/events", getEvents)
+
 	server.Run() // listens on 0.0.0.0:8080 by default
+}
+
+func getEvents(context *gin.Context) {
+	context.JSON(http.StatusAccepted, gin.H{
+		"message": "hello",
+	})
 }
